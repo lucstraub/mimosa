5bd2b171b333c66257e05c22319da9d46c8dd258
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-02-21 12:37:26 +0100

    adjusted run.py to test multiple industry scaling parameters (temporary)

--- run.py
+++ run.py
@@ -5,6 +5,17 @@ params = load_params()
 # Make changes to the params if needed
 params["emissions"]["carbonbudget"] = False
 
-model1 = MIMOSA(params)
-model1.solve()
-model1.save("run1")
+# model1 = MIMOSA(params)
+# model1.solve()
+# model1.save("run1")
+
+#sector-feature
+for scaling_industry in [0.2, 0.8]:
+    params["industry"]["industry_scaling_baseline"] = scaling_industry
+    params["emissions"]["baseline carbon intensity"] = False
+
+    model1 = MIMOSA(params)
+    model1.solve()
+    model1.save(f"testrun_scaling_{scaling_industry}")
+    
+# model1.plot(filename="result")
