4f8d12fa1fdcb0b56cd8374c73618e5f952076b1
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-04-22 16:07:46 +0200

    adapted run.py file

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from mimosa import MIMOSA, load_params
 
 params = load_params()
@@ -10,12 +11,13 @@ params["emissions"]["carbonbudget"] = False
 # model1.save("run1")
 
 #sector-feature
-for scaling_industry in [0.2, 0.8]:
+for scaling_industry in [0.25]:
     params["industry"]["industry_scaling_baseline"] = scaling_industry
     params["emissions"]["baseline carbon intensity"] = False
 
     model1 = MIMOSA(params)
     model1.solve()
-    model1.save(f"testrun_sector_feature_industry_scaling_{scaling_industry}")
+    #model1.solve(use_neos=True, neos_email="l.straub@uu.nl")
+    model1.save(f"testrun_sector_feature_industry_scaling_{scaling_industry}_{datetime.today().strftime('%Y-%m-%d')}")
     
 # model1.plot(filename="result")
