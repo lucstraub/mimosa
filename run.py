c758f83f26edadb07571246fdffb0849224d667c
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-05-22 14:57:35 +0200

    update industry scaling parameter to IPCC calibrated data

--- run.py
+++ run.py
@@ -8,17 +8,9 @@ params = load_params()
 params["emissions"]["carbonbudget"] = False
 # params["emissions"]["baseline carbon intensity"] = False
 
-# model1 = MIMOSA(params)
-# model1.solve()
-# model1.save("run1")
-
-#sector-feature
-for scaling_industry in [0.25]:
-    params["industry"]["industry_scaling_baseline"] = scaling_industry
-    
-    model1 = MIMOSA(params)
-    model1.solve()
-    #model1.solve(use_neos=True, neos_email="l.straub@uu.nl")
-    model1.save(f"testrun_industry_scaling_{scaling_industry}_with_CE_{datetime.today().strftime('%Y-%m-%d')}")
+model1 = MIMOSA(params)
+model1.solve()
+# model1.solve(use_neos=True, neos_email="l.straub@uu.nl")
+model1.save(f"testrun_industry_with_CE_dummy_MAC_curves_{datetime.today().strftime('%Y-%m-%d')}")
     
 # model1.plot(filename="result")
