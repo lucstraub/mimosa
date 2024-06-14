c09ba89767baf3fcfc3ad1f58467308396880ad4
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-06-14 11:51:22 +0200

    minor description change

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ params["emissions"]["baseline carbon intensity"] = False
 params["emissions"]["inertia"]["global"] = -0.05
 params["emissions"]["inertia"]["regional"] = False
 params["emissions"]["regional min level"] = False
-params["emissions"]["non increasing emissions after 2100"] = False
+params["emissions"]["non increasing emissions after 2100"] = False # changed constraint from regional to global
 
 model1 = MIMOSA(params)
 model1.solve()
