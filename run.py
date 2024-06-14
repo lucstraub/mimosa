7180f14b06e08d7df7b5761f9d6d23ed2f0d8812
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-06-14 11:19:02 +0200

    updated industry share based on corrected calibration (industry emissions from demand, processes and electricity)

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ params = load_params()
 
 # Make changes to the params if needed
 params["emissions"]["carbonbudget"] = False
-# params["emissions"]["baseline carbon intensity"] = False
+params["emissions"]["baseline carbon intensity"] = False
 params["emissions"]["inertia"]["global"] = -0.05
 params["emissions"]["inertia"]["regional"] = False
 params["emissions"]["regional min level"] = False
@@ -15,6 +15,6 @@ params["emissions"]["non increasing emissions after 2100"] = False
 model1 = MIMOSA(params)
 model1.solve()
 # model1.solve(use_neos=True, neos_email="l.straub@uu.nl")
-model1.save(f"testrun_industry_with_CE_dummy_MAC_curves_{datetime.today().strftime('%Y-%m-%d-%H-%M')}")
+model1.save(f"testrun_industry{params['industry']['industry_scaling_baseline']}_with_CE_dummy_MAC_curves_run{datetime.today().strftime('%Y-%m-%d-%H-%M')}")
     
 # model1.plot(filename="result")
