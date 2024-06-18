b93c779e6fa7204697ad90d45f8c1857eeab39e0
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-06-18 13:10:51 +0200

    introduced basic material production scaling factor for industry baseline emissions

--- run.py
+++ run.py
@@ -7,14 +7,16 @@ params = load_params()
 # Make changes to the params if needed
 params["emissions"]["carbonbudget"] = False
 params["emissions"]["baseline carbon intensity"] = False
+# params["time"]["dt"] = 10
 params["emissions"]["inertia"]["global"] = -0.05
 params["emissions"]["inertia"]["regional"] = False
 params["emissions"]["regional min level"] = False
-params["emissions"]["non increasing emissions after 2100"] = False # changed constraint from regional to global
+params["emissions"]["non increasing emissions after 2100"] = True # changed constraint from regional to global
+params['industry']['basic_material_scaling_baseline'] = 0.66 # based on Material Economics
 
 model1 = MIMOSA(params)
 model1.solve()
 # model1.solve(use_neos=True, neos_email="l.straub@uu.nl")
-model1.save(f"testrun_industry{params['industry']['industry_scaling_baseline']}_with_CE_MAC_curves0.4_run{datetime.today().strftime('%Y-%m-%d-%H-%M')}")
+model1.save(f"testrun_industry{params['industry']['industry_scaling_baseline']}x{params['industry']['basic_material_scaling_baseline']}_with_CE_MAC_curves0.4_run{datetime.today().strftime('%Y-%m-%d-%H-%M')}")
     
 # model1.plot(filename="result")
