882aef8be59c9438b55f07fa2b046e34e787818b
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-06-18 15:37:08 +0200

    slightly adjusted run file

--- run.py
+++ run.py
@@ -4,7 +4,9 @@ params = load_params()
 
 # Make changes to the params if needed
 params["emissions"]["carbonbudget"] = False
+# params["emissions"]["carbonbudget"] = "500 GtCO2"
 
 model1 = MIMOSA(params)
 model1.solve()
 model1.save("run1")
+# model1.save("run1_budget500")
