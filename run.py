bf7d6ad08cc13c95d621788e4491b5b8a25d8476
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-06-18 13:14:55 +0200

    enabled global non increasing emissions after 2100 constraint

--- run.py
+++ run.py
@@ -7,10 +7,11 @@ params = load_params()
 # Make changes to the params if needed
 params["emissions"]["carbonbudget"] = False
 params["emissions"]["baseline carbon intensity"] = False
+# params["time"]["dt"] = 10
 params["emissions"]["inertia"]["global"] = -0.05
 params["emissions"]["inertia"]["regional"] = False
 params["emissions"]["regional min level"] = False
-params["emissions"]["non increasing emissions after 2100"] = False # changed constraint from regional to global
+params["emissions"]["non increasing emissions after 2100"] = True # changed constraint from regional to global
 params['industry']['basic_material_scaling_baseline'] = 0.66 # based on Material Economics
 
 model1 = MIMOSA(params)
