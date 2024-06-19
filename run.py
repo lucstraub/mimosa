777bd93aeba930f9b262ce7a48497e836acd66e4
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-06-19 16:28:56 +0200

    adjusted timeline for carbon budget run until 2100

--- run.py
+++ run.py
@@ -6,12 +6,13 @@ params = load_params()
 
 # Make changes to the params if needed
 params["emissions"]["carbonbudget"] = "500 GtCO2"
+params["time"]["end"] = 2100
 params["emissions"]["baseline carbon intensity"] = False
 # params["time"]["dt"] = 10
 params["emissions"]["inertia"]["global"] = -0.05
 params["emissions"]["inertia"]["regional"] = False
 params["emissions"]["regional min level"] = False
-params["emissions"]["non increasing emissions after 2100"] = True # changed constraint from regional to global
+params["emissions"]["non increasing emissions after 2100"] = False # changed constraint from regional to global
 
 model1 = MIMOSA(params)
 model1.solve()
