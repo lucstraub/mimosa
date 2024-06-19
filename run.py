d3912d1bcf947deaa4ecea22494700c410f1590a
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-06-19 16:39:35 +0200

    adjusted timline to 2100 for budget runs

--- run.py
+++ run.py
@@ -1,18 +1,19 @@
 from mimosa import MIMOSA, load_params
+from datetime import datetime
 
 params = load_params()
 
 # Make changes to the params if needed
-params["emissions"]["carbonbudget"] = False
-# params["emissions"]["carbonbudget"] = "500 GtCO2"
+params["emissions"]["carbonbudget"] = "500 GtCO2"
+params["time"]["end"] = 2100
 params["emissions"]["baseline carbon intensity"] = False
 # params["time"]["dt"] = 10
 params["emissions"]["inertia"]["global"] = -0.05
 params["emissions"]["inertia"]["regional"] = False
 params["emissions"]["regional min level"] = False
-params["emissions"]["non increasing emissions after 2100"] = True # changed constraint from regional to global
+params["emissions"]["non increasing emissions after 2100"] = False # changed constraint from regional to global
 
 model1 = MIMOSA(params)
 model1.solve()
-model1.save("base")
-# model1.save("base_budget500")
+# model1.solve(use_neos=True, neos_email="l.straub@uu.nl")
+model1.save(f"testrun_budget{params['emissions']['carbonbudget']}_base_run{datetime.today().strftime('%Y-%m-%d-%H-%M')}")
