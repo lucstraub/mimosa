232c21ab77bab20308a0b90054ddcf6fc80bf1dc
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-11-11 17:28:09 +0100

    merged with calibration branch/ included gamma scaling/ mitigation cost calibration code in run file

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+import os
+import numpy as np
+import pandas as pd
 from mimosa import MIMOSA, load_params
 from datetime import datetime
 
@@ -14,9 +17,64 @@ params["emissions"]["regional min level"] = False
 params["emissions"]["non increasing emissions after 2100"] = False # changed constraint from regional to global
 params["industry"]["gamma_scaling"] = 1.034
 
+#---------------------------------------------------
+# Uncomment below code for single scenario runs
+
 model1 = MIMOSA(params)
 model1.solve()
 # model1.solve(use_neos=True, neos_email="l.straub@uu.nl")
 model1.save(f"testrun_budget{params['emissions']['carbonbudget']}_industry{params['industry']['industry_scaling_baseline']}_with_nonCE_MAC_curves_gammascale{params['industry']['gamma_scaling']}_run{datetime.today().strftime('%Y-%m-%d-%H-%M')}")
     
 # model1.plot(filename="result")
+
+#---------------------------------------------------
+# Uncomment below code for total mitigation cost/ gamma scaling factor calibration runs in industry scenario
+
+# params["economics"]["damages"]["ignore damages"] = True
+ 
+# try:
+#     os.mkdir("output/calibration")
+# except FileExistsError:
+#     pass
+
+# # gamma_scaling_factors = np.arange(0.6, 1.5, 0.2)
+# # gamma_scaling_factors = [1.03, 1.04, 1.05]
+# gamma_scaling_factors = [1.034]
+# carbon_budgets = np.arange(200, 1501, 100)
+
+# for scaling in gamma_scaling_factors:
+#     params["industry"]["gamma_scaling"] = scaling
+
+#     for budget in carbon_budgets:
+#         params["emissions"]["carbonbudget"] = f"{budget} GtCO2"
+    
+#         model = MIMOSA(params)
+#         model.solve()
+#         model.save(f"calibration/with_gamma_scaling/industry_gamma_{scaling:.3f}_cb_{budget}")
+
+# # Calculate the NPV of the mitigation costs for each run:
+ 
+# def npv(values, discount_rate):
+#     years = values.index.astype(float)
+#     t = years - years[0]
+#     discount_factor = np.exp(-discount_rate * t)
+#     return np.trapz(values * discount_factor, t)
+
+# for scaling in gamma_scaling_factors:
+#     results = []
+
+#     print(f"Gamma scaling factor: {scaling:.3f}")
+
+#     for budget in carbon_budgets:
+#         outp = pd.read_csv(f"output/calibration/with_gamma_scaling/industry_gamma_{scaling:.3f}_cb_{budget}.csv")
+#         global_mitig_costs = outp.loc[outp["Variable"] == "mitigation_costs", "2020":].sum(
+#             axis=0
+#         )
+#         global_gdp_gross = outp.loc[outp["Variable"] == "GDP_gross", "2020":].sum(axis=0)
+#         r = 0.03
+#         npv_costs = npv(global_mitig_costs, r) / npv(global_gdp_gross, r)
+#         results.append({"budget": budget, "npv_costs": npv_costs})
+#         print(f"NPV of mitigation costs for {budget} GtCO2: {npv_costs:.1%}")
+
+#     results = pd.DataFrame(results)
+#     results.to_csv(f"output/calibration/with_gamma_scaling/industry_gamma_{scaling:.3f}_npv_costs.csv", index=False)
