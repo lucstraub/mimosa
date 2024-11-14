a4e1f6efaf539af8c623f5d3eaffd7f41c3d618e
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-11-14 13:49:48 +0100

    minor run.py adjustment

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from mimosa import MIMOSA, load_params
 from datetime import datetime
+# from pyomo.environ import value
 
 params = load_params()
 
@@ -21,6 +22,7 @@ params["industry"]["gamma_scaling"] = 1.034
 # Uncomment below code for single scenario runs
 
 model1 = MIMOSA(params)
+# print("Value of Gamma: ", value(model1.concrete_model.MAC_gamma))
 model1.solve()
 # model1.solve(use_neos=True, neos_email="l.straub@uu.nl")
 model1.save(f"testrun_budget{params['emissions']['carbonbudget']}_industry{params['industry']['industry_scaling_baseline']}_with_nonCE_MAC_curves_gammascale{params['industry']['gamma_scaling']}_run{datetime.today().strftime('%Y-%m-%d-%H-%M')}")
