1b9304d63b4e2542a5bfec415321610d99372993
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-11-28 13:53:45 +0100

    adjusted gamma scaling factor

--- run.py
+++ run.py
@@ -16,7 +16,7 @@ params["emissions"]["inertia"]["global"] = -0.05
 params["emissions"]["inertia"]["regional"] = False
 params["emissions"]["regional min level"] = False
 params["emissions"]["non increasing emissions after 2100"] = False # changed constraint from regional to global
-params["industry"]["gamma_scaling"] = 0.985
+params["industry"]["gamma_scaling"] = 0.99
 params["model"]["welfare module"] = "cost_minimising"
 
 #---------------------------------------------------
